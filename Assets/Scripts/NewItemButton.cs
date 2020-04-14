97ab9234f09c6bb159dd55250d313bf554393643
Noxalus <noxalus@gmail.com>
2020-04-14 15:20:14 +0200

    Create a new button to place a new item.

---
+++ Assets/Scripts/NewItemButton.cs
@@ -0,0 +1,54 @@
+﻿using GamePrototype;
+using ModTool.Interface;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewItemButton : ModBehaviour
+{
+    [SerializeField]
+    private GameObject _newItem = null;
+
+    private List<GameObject> _instances = new List<GameObject>();
+    private bool _isPlacingNewItem = false;
+
+    private void Start()
+    {
+        GameManager.Instance.ItemPlacer.OnItemPlaced += OnItemPlaced;
+        GameManager.Instance.ItemPlacer.OnItemChanged += OnItemChanged;
+    }
+
+    private void OnItemChanged(string itemName)
+    {
+        Debug.Log("Item selection changed");
+        _isPlacingNewItem = false;
+    }
+
+    private void OnItemPlaced(GameObject instance)
+    {
+        if(_isPlacingNewItem)
+        {
+            Debug.Log("Add instance to the list");
+            _instances.Add(instance);
+            Debug.Log($"Instance list size: {_instances.Count}");
+        }
+        else
+        {
+            Debug.Log("It's not a new item");
+        }
+    }
+
+    public void OnClick()
+    {
+        GameManager.Instance.SetItem(_newItem);
+        _isPlacingNewItem = true;
+        Debug.Log("Is placing new item");
+    }
+
+    private void OnDestroy()
+    {
+        foreach (GameObject instance in _instances)
+        {
+            Destroy(instance.gameObject);
+        }
+    }
+}
---
+++ Backup/Scripts/GamePrototypeMod.cs
@@ -0,0 +1,56 @@
+﻿using ModTool.Interface;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePrototypeMod : ModBehaviour
+{
+    [SerializeField]
+    private string _buttonsName = "Buttons";
+
+    [SerializeField]
+    private Button _button = null;
+
+    void Start()
+    {
+        GameObject buttons = FindInActiveObjectByName(_buttonsName);
+
+        if (buttons == null)
+        {
+            Debug.LogError($"Can't find object with name \"{_buttonsName}\"");
+        }
+        else
+        {
+            if (_button == null)
+            {
+                Debug.LogError("New item button is null");
+            }
+            else
+            {
+                _button.transform.SetParent(buttons.transform, false);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(_button.gameObject);
+    }
+
+    private GameObject FindInActiveObjectByName(string name)
+    {
+        Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
+
+        for (int i = 0; i < objs.Length; i++)
+        {
+            if (objs[i].hideFlags == HideFlags.None)
+            {
+                if (objs[i].name == name)
+                {
+                    return objs[i].gameObject;
+                }
+            }
+        }
+
+        return null;
+    }
+}
